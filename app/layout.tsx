8fb50ccb58a5045542b0fac7f275f5081f2a8b3c
jhean <brizadaojhean@gmail.com>
2023-10-28 21:31:52 -0200

    feat: Layout raiz

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
+import { inter } from "@/app/ui/fonts/fonts";
+import "@/app/ui/global.css";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='pt-br'>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
