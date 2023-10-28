8bf2bdbefbdec356ec2ed7169a127bf07164a27d
jhean <brizadaojhean@gmail.com>
2023-10-28 21:36:40 -0200

    feat:Lcompartilhado-rota dashboard-rotas-aninhadas

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+export default function LayoutDashboard({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
+      <div className='w-full flex-none md:w-64'>
+        <SideNav />
+      </div>
+      <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
+    </div>
+  );
+}
